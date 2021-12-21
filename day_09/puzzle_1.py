59e2f6d6c1c473ca451abd701c98942547982058
jiri.one <nemec@jiri.one>
2021-12-21 22:24:23 +0100

    finished day 9 part 1 (whole in numpy, this time I have good memory efficiency)

--- day_09/puzzle_1.py
+++ day_09/puzzle_1.py
@@ -1,17 +1,37 @@
 import numpy as np
 
-# result of test_input.txt file have to be 15 and for input.txt it is XXX
-with open("test_input.txt", "r") as file:
+# result of test_input.txt file have to be 15 and for input.txt it is 494
+with open("input.txt", "r") as file:
 	while line:=file.readline():
 		try:
 			arr = np.vstack((arr, np.array([int(x) for x in line.strip()], dtype=int)))
 		except NameError:
 			arr = np.array([int(x) for x in line.strip()], dtype=int)
 
+lowest_points = np.array([], dtype=int)
+
 for row_nr, row in enumerate(arr):
 	if row_nr == 0:
-		column_neighbors = (1)
+		row_neighbors = [1]
 	elif row_nr == len(arr)-1:
-		column_neighbors = (len(arr)-2)
+		row_neighbors = [len(arr)-2]
+	else: 
+		row_neighbors = [row_nr-1, row_nr+1]
 	for elm_nr, elm in enumerate(row):
+		neighbors_values = np.array([], dtype=int)
+		if elm_nr == 0:
+			neighbors_values = np.append(neighbors_values, row[1])
+		elif elm_nr == len(row)-1:
+			neighbors_values = np.append(neighbors_values, row[elm_nr-1])
+		else: 
+			neighbors_values = np.append(neighbors_values, row[elm_nr-1])
+			neighbors_values = np.append(neighbors_values, row[elm_nr+1])
 		
+		for row_index in row_neighbors:
+			neighbors_values = np.append(neighbors_values, arr[row_index][elm_nr])
+		
+		neighbors_values.sort()
+		if elm < sorted(neighbors_values)[0]:
+			lowest_points = np.append(lowest_points, elm)
+
+print(np.sum(lowest_points+1))

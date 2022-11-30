1192c86fc5959c0b927f2901c884695b0d39c3f7
jiri.one <nemec@jiri.one>
2022-11-30 20:29:42 +0100

    I am sick, so only initial commit

--- day_15/puzzle_1.py
+++ day_15/puzzle_1.py
@@ -16,10 +16,27 @@ with open(test_input, "r") as file:
         line = line.strip()
         if line:
             try:
-                arr = np.vstack((arr, np.array([int(x) for x in line], dtype=int)))
+                arr_risk = np.vstack((arr_risk, np.array([int(x) for x in line], dtype=int)))
+                arr_check = np.vstack((arr_check, np.array([True for x in line], dtype=bool)))
+                arr_way = np.vstack((arr_way, np.array([0 for x in line], dtype=int)))
+                
             except NameError:
-                arr = np.array([int(x) for x in line], dtype=int)
+                arr_risk = np.array([int(x) for x in line], dtype=int)
+                arr_check = np.array([True for x in line], dtype=bool)
+                arr_way = np.array([0 for x in line], dtype=int)
+                
 
-coor = (0, 0)
-while coor != arr.shape:
-    
+print(arr_risk)
+print(arr_check)
+print(arr_way)
+
+arr_check[0][0] = False # no need to check top left element
+
+
+with np.nditer([arr_risk, arr_check, arr_way], flags=['multi_index'], op_flags=['readwrite']) as ar:
+    while True in arr_check:
+        for risk, check, way in ar: # from is from arr_risk, check is from... etc
+            x, y = ar.multi_index
+            print(x, y)
+            # if y[...] == "#":
+            #     x[...] = "#"

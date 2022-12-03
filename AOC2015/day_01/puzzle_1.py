bcfb55080570aec87152c01e4e47f405a985522f
jiri.one <nemec@jiri.one>
2022-12-03 23:32:32 +0100

    finished both parts from day1_15

---
+++ AOC2015/day_01/puzzle_1.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+# input files
+main_input = Path(__file__).parent / "input.txt" # result of this file is 74
+
+
+# read the initial file
+with open(main_input, "r") as file:
+    line = file.readline().strip()
+    counter: int = 0
+    for bratchet in line:
+        if bratchet == "(":
+            counter += 1
+        else:
+            counter -= 1
+
+print(counter)
---
+++ AOC2015/day_01/puzzle_2.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+# input files
+main_input = Path(__file__).parent / "input.txt" # result of this file is 1795
+
+
+# read the initial file
+with open(main_input, "r") as file:
+    line = file.readline().strip()
+    counter: int = 0
+    for position, bratchet in enumerate(line, start=1):
+        if bratchet == "(":
+            counter += 1
+        else:
+            counter -= 1
+        if counter == -1:
+            print(position)
+            break

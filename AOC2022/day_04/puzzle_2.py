e796fc978bb870e84054e0a55eb3afc158995128
jiri.one <nemec@jiri.one>
2022-12-04 09:14:00 +0100

    finished day4p2_22

--- AOC2022/day_04/puzzle_2.py
+++ AOC2022/day_04/puzzle_2.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 
 # input files
-main_input = Path(__file__).parent / "input.txt" # result of this file is 562
-test_input = Path(__file__).parent / "test_input.txt" # result of this file is 2
-
-# helper functions
+main_input = Path(__file__).parent / "input.txt" # result of this file is 924
+test_input = Path(__file__).parent / "test_input.txt" # result of this file is 4
 
 # helper variables
 overlaps: int = 0
@@ -19,10 +17,11 @@ with open(main_input, "r") as file:
             elf2_start, elf2_end = [int(nr) for nr in elf2.split("-")]
             elf1_range = range(elf1_start, elf1_end + 1)
             elf2_range = range(elf2_start, elf2_end + 1)
-            if elf1_start in elf2_range and elf1_end in elf2_range:
+            if (    elf1_start in elf2_range
+                    or elf1_end in elf2_range
+                    or elf2_start in elf1_range
+                    or elf2_end in elf1_range):
                 overlaps += 1
-            elif elf2_start in elf1_range and elf2_end in elf1_range:
-                overlaps += 1
-            
+          
 
 print(overlaps)

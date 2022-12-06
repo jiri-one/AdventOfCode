50e4e2f45842dc77fbfc65d154cd8d5e1fda249e
jiri.one <nemec@jiri.one>
2022-12-06 15:53:46 +0100

    finished d15p2_22

--- AOC2022/day_05/puzzle_2.py
+++ AOC2022/day_05/puzzle_2.py
@@ -1,27 +1,66 @@
 from pathlib import Path
 
 # input files
-main_input = Path(__file__).parent / "input.txt" # result of this file is 924
-test_input = Path(__file__).parent / "test_input.txt" # result of this file is 4
+main_input = Path(__file__).parent / "input.txt" # result of this file is PWPWHGFZS
+test_input = Path(__file__).parent / "test_input.txt" # result of this file is MCD
 
 # helper variables
-overlaps: int = 0
+stacks: dict[int, list[str]] = {}
+raw_lines: list[str]
+move: list[dict[str, int]] = []
 
 # read the initial file
 with open(main_input, "r") as file:
-    for line in file:
-        line = line.strip()
-        if line:
-            elf1, elf2 = line.split(",")
-            elf1_start, elf1_end = [int(nr) for nr in elf1.split("-")]
-            elf2_start, elf2_end = [int(nr) for nr in elf2.split("-")]
-            elf1_range = range(elf1_start, elf1_end + 1)
-            elf2_range = range(elf2_start, elf2_end + 1)
-            if (    elf1_start in elf2_range
-                    or elf1_end in elf2_range
-                    or elf2_start in elf1_range
-                    or elf2_end in elf1_range):
-                overlaps += 1
-          
+    raw_lines = file.readlines()
 
-print(overlaps)
+# create stacks
+for line_index, line in enumerate(raw_lines):
+    if "[" not in line and "move" not in line: 
+        line_in_list = [x for x in line.split() if x] # split line to list and remove empty elements
+        try:
+            for stack in line_in_list:
+                stacks[int(stack)] = [] # all stacks are in dict with stack number key
+            raw_lines.pop(line_index) # remove line with stacks
+        except ValueError:
+            pass
+
+# create move commands
+while line := raw_lines.pop(-1):
+    if "move" not in line:
+        raw_lines.append(line) # return the move line back to the end
+        break
+    line = line.strip()
+    line_list = line.split()
+    move.append({
+        "crates_count": int(line_list[1]),
+        "src_stack": int(line_list[3]),
+        "dst_stack": int(line_list[5])
+    })
+
+# fill stacks
+while raw_lines:
+    line = raw_lines.pop(-1)
+    start, stop = 0, 3
+    stack = 1
+    while stop < len(line):
+        crate = line[start:stop]
+        crate = crate.strip()
+        if len(crate) != 0:
+            crate = crate.replace("[", "").replace("]", "")
+            stacks[stack].append(crate)
+        stack += 1
+        start += 4
+        stop += 4
+
+
+# move crates
+for cmd in reversed(move):
+    moved_crates = stacks[cmd["src_stack"]][-cmd["crates_count"]:]
+    stacks[cmd["src_stack"]] = stacks[cmd["src_stack"]][:-cmd["crates_count"]]
+    stacks[cmd["dst_stack"]] = stacks[cmd["dst_stack"]] + moved_crates
+
+final_chars = ""
+for stack in stacks.values():
+    final_chars += stack[-1]
+    
+print(final_chars)

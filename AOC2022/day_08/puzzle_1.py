08dbf733e8d325d1b40091142180db0fb9f5bedf
jiri.one <nemec@jiri.one>
2022-12-08 10:19:26 +0100

    finished day8p1_22

---
+++ AOC2022/day_08/puzzle_1.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import numpy as np
+
+# input files
+main_input = Path(__file__).parent / "input.txt" # result of this file is 1703
+test_input = Path(__file__).parent / "test_input.txt" # result of this file is 21
+
+# helper variables
+visible: int = 0
+
+# read the initial file
+with open(main_input, "r") as file:
+    while line:= file.readline():
+        line = line.strip()
+        try:
+            arr = np.vstack((arr, np.array([int(x) for x in line], dtype=int)))
+        except NameError:
+            arr = np.array([int(x) for x in line], dtype=int)
+
+size = np.shape(arr.T)
+with np.nditer(arr.T, flags=['multi_index']) as ar:
+    for elm in ar:
+        x, y = ar.multi_index
+        if x == 0 or y == 0 or x == size[0]-1 or y == size[1]-1:
+            visible += 1
+        else:
+            col = arr.T[x]
+            row = arr[y]
+            if (    np.all(col[:y] < elm)
+                or  np.all(col[y+1:] < elm)
+                or  np.all(row[:x] < elm)
+                or  np.all(row[x+1:] < elm)):
+                visible += 1
+
+print(visible)

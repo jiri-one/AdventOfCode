a2cedaa0f982b470b93292004964fb8e8285550d
jiri.one <nemec@jiri.one>
2022-12-22 15:45:00 +0100

    small improvements in parser for day19

--- AOC2022/day_19/puzzle_1.py
+++ AOC2022/day_19/puzzle_1.py
@@ -1,22 +1,19 @@
 from pathlib import Path
-from string import ascii_lowercase, ascii_uppercase
+from string import ascii_lowercase, ascii_uppercase, punctuation
 
 # input files
 main_input = Path(__file__).parent / "input.txt" # result of this file is XXX
 test_input = Path(__file__).parent / "test_input.txt" # result of this file is 33
 
 # helper variables
-unwanted_chars = ascii_lowercase + ascii_uppercase + ":."
+unwanted_chars = ascii_lowercase + ascii_uppercase + punctuation
 
 # helper functions
-    
 
 # read the initial file
 with open(main_input, "r") as file:
     while line:= file.readline():
-        line = line.strip()
-        for char in unwanted_chars:
-            line = line.replace(char, "")
+        for char in unwanted_chars: line = line.replace(char, "")
         line_list = line.split()
         blueprint = line_list[0]
         ore_robot = line_list[1]

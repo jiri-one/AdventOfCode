8e1b18843cecdf7d8a87e46586e36f6d0ec62f34
jiri.one <nemec@jiri.one>
2023-01-06 07:53:29 +0100

    some unfinished work of day_19p1

--- AOC2022/day_19/puzzle_1.py
+++ AOC2022/day_19/puzzle_1.py
@@ -4,6 +4,7 @@ from string import ascii_lowercase, ascii_uppercase, punctuation
 from dataclasses import dataclass
 from pprint import pprint
 from copy import deepcopy
+from collections import namedtuple
 
 # input files
 main_input = Path(__file__).parent / "input.txt" # result of this file is XXX
@@ -13,33 +14,54 @@ test_input = Path(__file__).parent / "test_input.txt" # result of this file is 3
 unwanted_chars = ascii_lowercase + ascii_uppercase + punctuation
 
 # helper functions and classes
+Price = namedtuple('Price', ['ore', 'clay', 'obsidian'])
+
 @dataclass
 class BluePrint:
     nr: int
-    prices: dict[str, tuple[int,int,int]] # material prices are: ore, clay, obsidian
+    prices: dict[str, Price[int,int,int]] # material prices are: ore, clay, obsidian
+
+
+Materials = namedtuple('Materials', ['ore', 'clay', 'obsidian', 'geode'])
+Robots = namedtuple('Robots', ['ore', 'clay', 'obsidian', 'geode'])
 
 # current blueprint state
-state: dict = {"materials": (0, 0, 0), # ore, clay, obsidian
-               "robots": (1, 0, 0, 0), # ore, clay, obsidian, geode robot; one ore robot for start
-               "blueprint": None,
-               "minutes_to_end": 24}
+state: dict = {"mats": Materials(0, 0, 0, 0), # materials: ore, clay, obsidian
+               "robs": Robots(1, 0, 0, 0), # robots: ore, clay, obsidian, geode
+               "bp": None, # BluePrint
+               "mins": 24} # minutest to the end
 
 blueprints = []
 answer = 0
 
 def cretate_states(state):
     # if I have material, buy robot
-    for index, price in state["blueprint"].prices.values():
-         minutes_to_end = state["minutest_to_end"]
-         materials = state["materials"]
-         robots = state["robots"]
+    for price in enumerate(state["bp"].prices.values()):
+         minutes_to_end = state["mins"]
+         mats = state["mats"]
+         robs = state["robs"]
          while minutes_to_end:
              minutes_to_end -= 1
-             materials = (
-                 materials[index] + robots[index], 
-             )
+             mats = Materials(  mats.ore + robs.ore,
+                                mats.clay + robs.clay,
+                                mats.obsidian + robs.obsidian,
+                                mats.geode + robs.geode)
+             print(minutes_to_end, mats)
     
     # if I don't have material, wait
+
+
+def get_best_score(blueprint, minutes):
+    global state
+    init = deepcopy(state)
+    init["bp"] = blueprint
+    init["mins"] = minutes
+    to_visit = [init]
+    while to_visit:
+        state = to_visit.pop()
+        cretate_states(init)
+    return 42
+
 
 # read the initial file
 with open(test_input, "r") as file:
@@ -49,21 +71,13 @@ with open(test_input, "r") as file:
         blueprint, ore_rob, clay_rob, obsidian_rob_ore, obsidian_rob_clay, geode_rob_ore, geode_rob_obs = line_list
         blueprint = BluePrint(
             nr = blueprint,
-            prices = {"ore_rob": (ore_rob, 0, 0),
-                      "clay_rob": (clay_rob, 0, 0),
-                      "obsidian_rob": (obsidian_rob_ore, obsidian_rob_clay, 0),
-                      "geode_rob": (geode_rob_ore, 0, geode_rob_obs)})
+            prices = {"ore_rob": Price(ore_rob, 0, 0),
+                      "clay_rob": Price(clay_rob, 0, 0),
+                      "obsidian_rob": Price(obsidian_rob_ore, obsidian_rob_clay, 0),
+                      "geode_rob": Price(geode_rob_ore, 0, geode_rob_obs)})
         blueprints.append(blueprint)
         pprint(blueprints)
 
-def get_best_score(blueprint, minutes):
-    init = deepcopy(state)
-    init["blueprint"] = blueprint
-    init["minutes_to_end"] = minutes
-    to_visit = [init]
-    while to_visit:
-        state = to_visit.pop()
-        cretate_states(init)
 
 for blueprint in blueprints:
     geodes = get_best_score(blueprint, 24)

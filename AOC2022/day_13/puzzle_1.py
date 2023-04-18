9ca5b80dacb51610f0ab2e7b2a4ea80559cf7afb
jiri.one <nemec@jiri.one>
2023-04-18 15:24:22 +0200

    begin of parser for input data

--- AOC2022/day_13/puzzle_1.py
+++ AOC2022/day_13/puzzle_1.py
@@ -1,12 +1,22 @@
 from pathlib import Path
 from sys import argv
 
-# set input file
+# set input file with data
 try:
     input_file = argv[1]
-    input_file = Path(__file__).parent / argv[1]  
+    input_file = Path(__file__).parent / argv[1]
+    if not input_file.exists():
+        raise FileNotFoundError() 
     # standard input files are "input.txt" and "test_input.txt"
     # # result of "input.txt" is XXX
     # # result of "test_input.txt" is 13
 except IndexError:
     print("use format: python file.py some_input_file")
+except FileNotFoundError:
+    print("Input file has to exist and the path has to be correct.")
+
+with open(input_file, "r") as file:
+    for line in file:
+        line = line.strip()
+        if line:
+            print(line)

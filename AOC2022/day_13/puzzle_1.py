4c9f9fe593cddea3efd04fe1dfbb560677ed573f
jiri.one <nemec@jiri.one>
2023-04-22 10:00:03 +0200

    some small progress

--- AOC2022/day_13/puzzle_1.py
+++ AOC2022/day_13/puzzle_1.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from itertools import zip_longest
 from sys import argv
 
 # set input file with data
@@ -24,9 +25,43 @@ with open(input_file, "r") as file:
         if line:
             line_counter +=1
             if line_counter % 2 == 0:
-                r_packets.append(line)
+                r_packets.append(eval(line))
             else:
-                l_packets.append(line) 
+                l_packets.append(eval(line)) 
 
-print(l_packets)
-print(r_packets)
+# print(l_packets)
+# print(r_packets)
+
+# helper functions and variables
+# def same_type(first, second):
+#     return type(first) 
+def compare_packets(l_packet, r_packet):
+    for l_elm, r_elm in zip_longest(l_packet, r_packet):
+        if type(l_elm) == type(r_elm): # both sides are same type
+            if isinstance(l_elm, int): # both sides are integer
+                if l_elm < r_elm:
+                    return True
+                if l_elm > r_elm:
+                    print("zde konec", l_elm, r_elm)
+                    return False
+            elif isinstance(l_elm, list): # both sides are list
+                print("a tady dva seznamy s jednickama", l_elm, r_elm)
+                if compare_packets(l_elm, r_elm):
+                    return True
+        else: # mixed types
+            if isinstance(l_elm, int):
+                l_elm = [l_elm] # convert l_elm to list
+            if isinstance(r_elm, int):
+                r_elm = [r_elm] # convert r_elm to list
+            return compare_packets(l_elm, r_elm)
+            
+            
+
+right_order = []
+for packet_index, (l_packet, r_packet) in enumerate(zip(l_packets[:3], r_packets[:3]), start=1):
+    print(packet_index, (l_packet, r_packet))
+    if compare_packets(l_packet, r_packet):
+        right_order.append(packet_index)
+
+print("right_order", right_order)
+print("answer part1:", sum(right_order))

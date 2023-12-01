c27ae7572afd4923694df5ceb10b496b252be288
jiri-one <nemec@jiri.one>
2023-12-01 10:49:35 +0100

    test input passed in day1_p1

---
+++ AOC2023/day_01/puzzle_1.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from sys import argv, exit
+
+# input files
+main_input = Path(__file__).parent / "input.txt"  # result of this file is XXX
+test_input = Path(__file__).parent / "test_input.txt"  # result of this file is 142
+
+if argv[1] == "--test":
+    main_input = test_input
+
+# helper variables
+values = []
+actual_calories = 0
+
+# read the initial file
+with open(main_input, "r") as file:
+    for line in file:
+        line = line.strip()
+        if len(line) != 0:
+            for char in line:
+                try:
+                    value_1 = int(char)
+                    break
+                except ValueError:
+                    pass
+            for char in reversed(line):
+                try:
+                    value_2 = int(char)
+                    break
+                except ValueError:
+                    pass
+            values.append(int(f"{value_1}{value_2}"))
+
+print(sum(values))

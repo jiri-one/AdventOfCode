f80433ca527f972e7bc503b61deab31ca9613572
jiri-one <nemec@jiri.one>
2023-12-03 17:15:07 +0100

    shorter solution and finished day2_p1

--- AOC2023/day_02/puzzle_1.py
+++ AOC2023/day_02/puzzle_1.py
@@ -3,26 +3,15 @@ from sys import argv
 from dataclasses import dataclass
 
 # input files
-main_input = Path(__file__).parent / "input.txt"  # result of this file is XXX
+main_input = Path(__file__).parent / "input.txt"  # result of this file is 2348
 test_input = Path(__file__).parent / "test_input.txt"  # result of this file is 8
 
 if len(argv) > 1 and argv[1] == "--test":
     main_input = test_input
 
 # helper variables
-@dataclass
-class Round:
-    red: int = 0
-    green: int = 0
-    blue: int = 0
-
-    def __gt__(self, other):
-        return self.red > other.red and self.green > other.green and self.blue > other.blue
-    
-    def __lt__(self, other):
-        return self.red < other.red and self.green < other.green and self.blue < other.blue
-
-cubes = Round(**{"red": 12, "green": 13, "blue": 14})
+cubes = {"red": 12, "green": 13, "blue": 14}
+games = []
 
 impossible_games = set()
 
@@ -30,21 +19,21 @@ impossible_games = set()
 with open(main_input, "r") as file:
     for line in file:
         line = line.strip()
+        print(line)
         if len(line) != 0:
             game_nr = int(line.split(":")[0].removeprefix("Game "))
+            games.append(game_nr)
             raw_game = [r.strip() for r in line.split(":")[1].strip().split(";")]
             rounds = []
+            game = []
             for raw_round in raw_game:
-                round = Round()
-                cobes_in_round = raw_round.strip().split(", ")
-                for one_color in cobes_in_round:
+                cubes_in_round = raw_round.strip().split(", ")
+                for one_color in cubes_in_round:
                     nr_of_cubes, color_of_cubes = one_color.split()
-                    setattr(round, color_of_cubes, int(nr_of_cubes))
-                if cubes > round:
-                    continue
-                else:
-                    impossible_games.add(game_nr)
+                    real_cubes_in_this_color = cubes.get(color_of_cubes)
+                    if real_cubes_in_this_color < int(nr_of_cubes):
+                        impossible_games.add(game_nr)
 
-possible_games = [game for game in range(1, game_nr+1) if game not in impossible_games]
+
+possible_games = [game for game in games if game not in impossible_games]
 print(sum(possible_games))
-print("--------------------------------")

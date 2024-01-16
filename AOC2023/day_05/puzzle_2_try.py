1e16c748fce022f0f7865352681554dd5ad774c6
jiri-one <nemec@jiri.one>
2024-01-16 16:17:29 +0100

    not working solution

--- AOC2023/day_05/puzzle_2_try.py
+++ AOC2023/day_05/puzzle_2_try.py
@@ -14,7 +14,7 @@ if len(argv) > 1 and argv[1] == "--test":
 class Map:
     src: str
     dst: str
-    ranges: list[tuple[range, range]] = field(default_factory=list) # tuple is src int, dst int, range int
+    ranges: list[tuple[range, range]] = field(default_factory=list) # tuple is src range and dst range
 
 inputs = list()
 maps: list[Map] = list()
@@ -46,22 +46,52 @@ def sort_ranges(ranges: list[range]):
     new_ranges = sorted(ranges, key=lambda r: r.start)
     return new_ranges
 
+def reduce_ranges(ranges: list[range]):
+    index = 0
+    while True:
+        if index >= len(ranges)-1:
+            break
+        #print(index, ranges)
+        r1 = ranges.pop(index)
+        r2 = ranges.pop(index)
+        
+        if r1.stop < r2.start:
+            ranges.insert(index, r1)
+            ranges.insert(index+1, r2)
+            index += 1
+            continue
+        
+        if r2.start in r1 and r2[-1] in r1:
+            ranges.insert(index, r1)
+            continue
 
-lowest_location = 1000000000000
+        if (r2.start in r1 or r2.start == r1.stop) and r2.stop > r1.stop:
+            new_range = range(r1.start, r2.stop)
+            ranges.insert(index, new_range)
+            continue
+    
+    return ranges
+
+def optimize_ranges(ranges: list[range]):
+    sorted_ranges = sort_ranges(ranges)
+    return reduce_ranges(sorted_ranges)
+
+inputs = [range(82,83)]
 for m in maps:
     print("MAPS:", m.src, "to", m.dst)
     print("==============================")
     for rng in m.ranges:
         src_range, dst_range = rng
-        move = dst_range[0] - src_range[0]
+        move = dst_range.start - src_range.start
         print("src_range", src_range, "dst_range", dst_range)
         print("move", move)
+        print("current input for map:", inputs)
         new_inputs = []
         try:
             while inp:= inputs.pop(0):
                 print("input range", inp)
                 if inp.start in src_range and inp[-1] in src_range:
-                    print("completely in")
+                    print("input completely in src_range")
                     new_range = range(inp.start+move, inp.stop+move)
                     new_inputs.append(new_range)
                 
@@ -74,13 +104,24 @@ for m in maps:
                     print("comletely out")
                     new_inputs.append(inp)
                 
+                elif src_range.start in inp and src_range[-1] in inp:
+                    print("src_range completely in input map")
+                    new_range = range(src_range.start+move, src_range.stop+move)
+                    rest_of_range_to_start = range(inp.start, src_range.start)
+                    rest_of_range_from_end = range(src_range.stop, inp.stop)
+                    new_inputs.append(new_range)
+                    new_inputs.append(rest_of_range_to_start)
+                    new_inputs.append(rest_of_range_from_end)
+                    
+                    assert len(new_range) + len(rest_of_range_to_start) + len(rest_of_range_from_end) == len(inp)
+                                   
                 elif inp.start in src_range and inp[-1] not in src_range:
                     print("start is in src_range, end is not")
                     new_range = range(inp.start+move, src_range.stop+move)
                     rest_of_range = range(src_range.stop, inp.stop)
                     
                     new_inputs.append(new_range)
-                    inputs.append(rest_of_range)
+                    new_inputs.append(rest_of_range)
                     print(len(new_range), len(rest_of_range), len(inp))
                     print(new_range, rest_of_range, inp)
                     assert len(new_range) + len(rest_of_range) == len(inp)
@@ -92,13 +133,15 @@ for m in maps:
                     print("rest_of_range[-1]", rest_of_range[-1], "new_range[0]", new_range[0])
                     
                     new_inputs.append(new_range)
-                    inputs.append(rest_of_range)
+                    new_inputs.append(rest_of_range)
                     print(len(new_range), len(rest_of_range), len(inp))
                     print(f"{new_range=}, {rest_of_range=}, {inp=}")
                     assert len(new_range) + len(rest_of_range) == len(inp)
+
         except IndexError:
             print("new_inputs", new_inputs)
-            inputs = new_inputs
+            inputs = optimize_ranges(new_inputs)
+            print("optimized inputs", inputs)
     
     
 print(min([x[0] for x in new_inputs]))
--- AOC2023/day_05/test_puzzle2.py
+++ AOC2023/day_05/test_puzzle2.py
@@ -1,10 +1,16 @@
-from puzzle_2_try import sort_ranges
+from puzzle_2_try import sort_ranges, reduce_ranges
 from random import shuffle
 
-RANGES = [range(0, 39), range(1, 20), range(3, 60), range(70, 120), range(150, 160)]
+RANGES = [range(0, 39), range(1, 20), range(3, 60), range(70, 120), range(120, 130), range(150, 160)]
+REDUCED_RANGES = [range(0, 60), range(70, 130), range(150, 160)]
 
 def test_sort_ranges():
     shuffle_ranges = RANGES[:]
     shuffle(shuffle_ranges)
     sorted_ranges = sort_ranges(shuffle_ranges)
     assert sorted_ranges == RANGES
+
+def test_reduce_ranges():
+    origin_ranges = RANGES[:]
+    reduced_ranges = reduce_ranges(origin_ranges)
+    assert reduced_ranges == REDUCED_RANGES
